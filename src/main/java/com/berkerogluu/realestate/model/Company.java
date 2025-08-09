d010a04e38719786c9fd2540d9a0cb341d2610a9
scispow <scispow@gmail.com>
2025-08-09 22:43:56 +0300

    core structure; getters, setters and property fields on Company.java

--- src/main/java/com/berkerogluu/realestate/model/Company.java
+++ src/main/java/com/berkerogluu/realestate/model/Company.java
@@ -1,10 +1,152 @@
 package com.berkerogluu.realestate.model;
 
 import jakarta.persistence.*;
+import java.time.Instant;
 
 @Entity
 @Table(name = "company") // Used a singular table name because app will be managed by a single company
 public class Company {
+    // Encapsulation
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.Identity)
+    private Long id;
+
+    @Column(name = "company_name", nullable = false, length = 255)
+    private String companyName;
+
+    @Column(name = "contact_person", nullable = false, length = 255)
+    private String contactPerson;
+
+    @Column(name = "address", nullable = false, length = 500)
+    private String address;
+
+    @Column(name = "phone_number", length = 16)
+    private String phoneNumber;
+
+    @Column(name = "fax_number", length = 16)
+    private String faxNumber;
+
+    @Column(name = "email", length = 255)
+    private String email;
+
+    @Column(name = "website", length = 255)
+    private String website;
+
+    // https://ttbs.gtb.gov.tr/Home/BelgeSorgula
+    // https://ticaret.gov.tr/kurumsal-haberler/elektronik-ilan-dogrulama-sistemi-eids-yetki-dogrulama-uygulamasi-hayata-gecirildi
+    @Column(name = "trade_authorization_number", nullable = false, length = 255)
+    private String tradeAuthorizationNumber;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    public Company() {
+
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = Instant.now();
+        updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = Instant.now();
+    }
+
+    // Getters
+    public long getId() {
+        return id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public String getTradeAuthorizationNumber() {
+        return tradeAuthorizationNumber;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public void setTradeAuthorizationNumber(String tradeAuthorizationNumber) {
+        this.tradeAuthorizationNumber = tradeAuthorizationNumber;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
 
